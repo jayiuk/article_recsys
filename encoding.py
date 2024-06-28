1e3c3db8e4cc09151ca2c45adf9f3c07c078d61d
jayiuk <jayiuk987@gmail.com>
2024-06-28 17:49:02 +0900

    수정중

--- encoding.py
+++ encoding.py
@@ -4,9 +4,13 @@ class encoding:
         self.encoder = encoder
 
     def vocab(input):
+        vocab = {}
         word_list = []
         for word in input:
             word = word.lower()
             word_list.append(word)
+            if word not in vocab:
+                vocab[word] = 0
+            vocab[word] += 1
         return vocab
     

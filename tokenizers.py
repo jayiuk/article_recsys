8b39676d74994d79f997190e3e695feb7444d118
jayiuk <jayiuk987@gmail.com>
2024-06-28 14:27:16 +0900

    클래스로 만듬

--- tokenizers.py
+++ tokenizers.py
@@ -3,27 +3,24 @@ from nltk.tokenize import wordpunct_tokenize
 from nltk.tokenize import sent_tokenize
 import pandas as pd
 
-def word_tokenizer(tokenizer, input):
-    word_input = input.to_string()
-    word_token = tokenizer(word_input)
-    return word_token
+class tokenizers:
+    def word_tokenizer(tokenizer, input):
+        word_input = input.to_string()
+        word_token = tokenizer(word_input)
+        return word_token
 
-def sentword_tokenizer(sent2wordtokenizer, input):
-    sent2word = sent2wordtokenizer(input)
-    return sent2word
+    def sentence_tokenizer(sentokenizer, input, wordtokenizer):
+        sentence_input = input.to_string()
+        sentence_out = sentokenizer(sentence_input)
+        sentence2word_token = [wordtokenizer(sentence) for sentence in sentence_out]
+        return sentence2word_token
 
-def sentence_tokenizer(sentokenizer, input, wordtokenizer):
-    sentence_input = input.to_string()
-    sentence_out = sentokenizer(sentence_input)
-    sentence2word_token = [sentword_tokenizer(wordtokenizer, sentence) for sentence in sentence_out]
-    return sentence2word_token
-
-def stemming(stemer, input):
-    stem_out = [stemer.stem(word) for word in input]
-    return stem_out
+    def stemming(stemer, input):
+        stem_out = [stemer.stem(word) for word in input]
+        return stem_out
 
 data = pd.read_csv('./view_arti.csv.')
-word1 = word_tokenizer(wordpunct_tokenize, data['Title'])
-sent1 = sentence_tokenizer(sent_tokenize, data['Content'], wordpunct_tokenize)
-print(word1)
-print(sent1)
+word1 = tokenizers.word_tokenizer(wordpunct_tokenize, data['Title'])
+sent1 = tokenizers.sentence_tokenizer(sent_tokenize, data['Content'], wordpunct_tokenize)
+print(type(word1))
+print(type(sent1))

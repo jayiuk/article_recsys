8b344c5115ff64050aaeafd4e5a75b470a4667d4
jayiuk <jayiuk987@gmail.com>
2024-06-28 15:24:07 +0900

    인코딩 클래스 만드는 중

---
+++ encoding.py
@@ -0,0 +1,9 @@
+class encoding:
+    def __init__(self, input, encoder):
+        self.input = input
+        self.encoder = encoder
+
+    def encoding(input, encoder):
+        encoded = encoder(input)
+        return encoded
+    
--- text_preprocessing.py
+++
@@ -1,4 +0,0 @@
-class encoding:
-    def __init__(self, input, encoder):
-        self.input = input
-        self.encoder = encoder

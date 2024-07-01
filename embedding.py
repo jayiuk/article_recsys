1724726ed367d99b76ab362d1045d83085f1e143
jayiuk <jayiuk987@gmail.com>
2024-07-01 16:40:25 +0900

    embedding클래스 수정.

--- embedding.py
+++ embedding.py
@@ -20,7 +20,6 @@ tokenizerr = tokenizers
 data = pd.read_csv('./view_arti.csv.')
 word1 = tokenizerr.word_tokenizer(wordpunct_tokenize, data['Title'])
 sent1 = tokenizerr.sentence_tokenizer(sent_tokenize, data['Content'], wordpunct_tokenize)
-model = Word2Vec(sentences = word1, vector_size = 100, window = 5, min_count = 5, workers = 4, sg = 0)
-
-out = embedding.embed(model, word1)
-print(out.shape)
+word_sent = word1 + sent1
+model = Word2Vec(sentences = word_sent, vector_size = 100, window = 5, min_count = 5, workers = 4, sg = 0)
+model.save('w2v_model')
---
+++ word_embedding.py
@@ -0,0 +1,5 @@
+class embedding:
+    def __init__(self, model, train_word, word_input):
+        self.model = model
+        self.train_word = train_word
+        self.word_input = word_input

a59c98afeabf4266b5a461a1e7c4d495a426a5d7
jayiuk <jayiuk987@gmail.com>
2024-07-01 10:42:32 +0900

    w2v으로 할듯

---
+++ embedding.py
@@ -0,0 +1,7 @@
+class embedding:
+    def __init__(self, input, embedder):
+        self.input = input
+        self.embedder = embedder
+
+    def embed(embedder, input):
+        w2v = embedder(input)

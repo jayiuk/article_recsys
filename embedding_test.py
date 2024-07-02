d3e6b699a1e38a873afd03f9d443214d3eb6745d
jayiuk <jayiuk987@gmail.com>
2024-07-02 17:49:00 +0900

    embedding & modeling

--- embedding_test.py
+++ embedding_test.py
@@ -6,13 +6,11 @@ from gensim.models import KeyedVectors
 from nltk.tokenize import wordpunct_tokenize
 from nltk.tokenize import sent_tokenize
 from tokenizers import tokenizers
-from word_embedding import embedding
 
 
 tokenizerr = tokenizers
-model = Word2Vec
+model = Word2Vec()
+m = KeyedVectors(vector_size = 5)
 data = pd.read_csv('./view_arti.csv.')
 word1 = tokenizerr.word_tokenizer(wordpunct_tokenize, data['Title'])
 sent1 = tokenizerr.sentence_tokenizer(sent_tokenize, data['Content'], wordpunct_tokenize)
-word_sent = word1 + sent1
-w2v = embedding.make_w2v(model, word_sent)
--- embedding_test2.py
+++ embedding_test2.py
@@ -1,3 +1,4 @@
 from gensim.models import Word2Vec
 model = Word2Vec.load('w2v_model')
 print(model.wv.vectors.shape)
+print(model.wv.vectors)

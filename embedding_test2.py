bbf5c760706bacdf8433bdf36da21ef780eee877
jayiuk <jayiuk987@gmail.com>
2024-07-02 16:45:12 +0900

    modeling

--- embedding_test2.py
+++ embedding_test2.py
@@ -1,3 +1,3 @@
 from gensim.models import Word2Vec
 model = Word2Vec.load('w2v_model')
-print(model.wv.vectors)
+print(model.wv.vectors.shape)
--- model.py
+++ model.py
@@ -4,40 +4,18 @@ import torch.optim as optim
 import numpy as np
 import torch.nn.init as init
 
-class CNN(nn.Module):
-    def __init__(self, input_channels, mid_channels, output_channels, kernel_size, stride, padding):
-        super().__init__()
-        self.input_channels = input_channels
-        self.mid_channels = mid_channels
-        self.output_channels = output_channels
-        self.kernel_size = kernel_size
-        self.stride = stride
-        self.padding = padding
-
-    def conv1d_1(input_channels, output_channels, kernel_size):
-        model = nn.Sequential(
-            nn.Conv1d(input_channels, output_channels, kernel_size),
-            nn.ReLU()
-        )
-        return model
-    
-    def conv1d_2(input_channels, mid_channels, output_channels, kernel_size, stride):
-        model = nn.Sequential(
-            nn.Conv1d(input_channels, mid_channels, kernel_size, stride),
+class CNNlstm(nn.Module):
+    def __init__(self):
+        super(CNNlstm, self).__init__()
+        self.layer_1 = nn.Sequential(
+            nn.Conv1d(5, 3, 3),
             nn.ReLU(),
-            nn.Conv1d(mid_channels, output_channels, kernel_size, stride),
-            nn.ReLU()
+            nn.MaxPool1d(3, 1, 0)
         )
-        return model
-    
-    def maxpool1d(input_channels, output_channels, kernel_size, stride, padding):
-        pooling = nn.Sequential(
-            nn.MaxPool1d(input_channels, output_channels, kernel_size, stride, padding),
-            nn.ReLU()
+        self.layer_2 = nn.Sequential(
+            nn.Conv1d()
         )
-        return pooling
-    
-    def lstm():
-    
+
+   
     def forward(x):
-        
+        out = conv1d_1(x)
